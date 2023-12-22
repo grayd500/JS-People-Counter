1b542aacc5367448f15374abae2e223b491ec2c8
David <grayd500@gmail.com>
2023-12-22 14:32:10 -0500

    Update with even more css styling

--- index.js
+++ index.js
@@ -14,3 +14,8 @@ function save() {
     count = 0
 }
 
+
+// Photo by <a href="https://unsplash.com/@fellowferdi?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Ferdinand Stöhr</a> on <a href="https://unsplash.com/photos/flock-of-birds-flying-q0tQjy5KQAY?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+// Blue Photo by <a href="https://unsplash.com/@dastan_khdir?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Dastan khdir</a> on <a href="https://unsplash.com/photos/a-flock-of-seagulls-flying-over-a-body-of-water-uyn-RzT-Utw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
